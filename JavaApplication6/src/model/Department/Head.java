56c3bcaa0b181e1ac07ccf9b42ed969d73c37fbe
suprith-6 <kambadahalliprakas.s@northeastern.edu>
2022-12-11 00:58:14 -0500

    dept+emp

--- JavaApplication6/src/model/Department/Head.java
+++ JavaApplication6/src/model/Department/Head.java
@@ -1,13 +1,47 @@
 /*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
  */
 package model.Department;
 
+import model.Bed.Bed;
+import model.Organization.Organization;
+import model.Role.Role.RoleType;
+import java.util.List;
+
 /**
  *
- * @author sushmithaprathap
+ * @author Suprith
  */
 public class Head {
+    private RoleType staffType;
+    private List<Bed> bedList;
+    private List<Organization> orgList;
+
+    public RoleType getStaffType() {
+        return staffType;
+    }
+
+    public void setStaffType(RoleType staffType) {
+        this.staffType = staffType;
+    }
+
+    public List<Bed> getBedList() {
+        return bedList;
+    }
+
+    public void setBedList(List<Bed> bedList) {
+        this.bedList = bedList;
+    }
+
+    public List<Organization> getOrgList() {
+        return orgList;
+    }
+
+    public void setOrgList(List<Organization> orgList) {
+        this.orgList = orgList;
+    }
+    
     
 }
