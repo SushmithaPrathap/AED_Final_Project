07dee40bdbd6de58d19a123c82075549c8bfe334
Sushmitha Prathap <prathap.s@northeastern.edu>
2022-12-11 01:09:37 -0500

    enterprise crud

--- JavaApplication6/src/model/Enterprise/Enterprise.java
+++ JavaApplication6/src/model/Enterprise/Enterprise.java
@@ -4,10 +4,72 @@
  */
 package model.Enterprise;
 
+import model.Network.Network;
+import model.Organization.Organization;
+import model.Organization.OrganizationDirectory;
+
 /**
  *
  * @author sushmithaprathap
  */
-public class Enterprise {
-    
+public abstract class Enterprise extends Organization {
+
+    private EnterpriseType enterpriseType;
+    private OrganizationDirectory organizationDirectory;
+    private Network network;
+
+    public Network getNetwork() {
+        //networkName="Boston";
+        return this.network;
+    }
+
+    public void setNetwork(Network network) {
+        this.network = network;
+    }
+
+    public OrganizationDirectory getOrganizationDirectory() {
+        return organizationDirectory;
+    }
+
+    public enum EnterpriseType {
+        Hospital("Hospital"),
+        Pharmacy("Pharmacy"),
+        Lab("Lab"),
+        Insurance("Insurance");
+
+        private String value;
+
+        private EnterpriseType(String value) {
+            this.value = value;
+        }
+
+        public String getValue() {
+            return value;
+        }
+
+        @Override
+        public String toString() {
+            return value;
+        }
+    }
+
+    public EnterpriseType getEnterpriseType() {
+        return enterpriseType;
+    }
+
+    public void setEnterpriseType(EnterpriseType enterpriseType) {
+        this.enterpriseType = enterpriseType;
+    }
+
+    public Enterprise(String name, EnterpriseType type) {
+        super(name);
+        this.enterpriseType = type;
+        organizationDirectory = new OrganizationDirectory();
+    }
+
+    @Override
+    public String toString() {
+        return this.getName();
+    }
+
 }
--- JavaApplication6/src/model/Enterprise/HospitalEnterprise/Hospital.java
+++ JavaApplication6/src/model/Enterprise/HospitalEnterprise/Hospital.java
@@ -4,10 +4,38 @@
  */
 package model.Enterprise.HospitalEnterprise;
 
+//import model.Department.DepartmentDirectory;
+import model.Enterprise.Enterprise;
+//import model.Location.Location;
+//import model.Nurse.NurseDirectory;
+//import model.Patient.PatientDirectory;
+//import model.Department.Receptionist;
+//import model.Person.EmployeeDirectory;
+import Business.Role.Role;
+import java.util.ArrayList;
+
 /**
  *
  * @author sushmithaprathap
  */
-public class Hospital {
+public class Hospital extends Enterprise {
+    int id;
+    //EmployeeDirectory doctorList;
+    //NurseDirectory nurseList;
+    //Location location;
+    //DepartmentDirectory departmentList;
+    //PatientDirectory patientList;
+    //Receptionist receptionist;
+    
+    
+    //String enterpriseType;
+    public Hospital(String name){
+        super(name,EnterpriseType.Hospital);
+    }
+    @Override
+    public ArrayList<Role> getSupportedRole() {
+        return null;
+    }
+    
     
 }
